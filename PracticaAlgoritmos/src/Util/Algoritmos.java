82ce6c884f4689553a7816d6dab197edb7fb224b
Aitoritis <alb0026@alu.medac.es>
2024-04-19 16:33:46 +0200

    Fibonacci

---
+++ PracticaAlgoritmos/src/Util/Algoritmos.java
@@ -0,0 +1,37 @@
+package Util;
+
+public abstract class Algoritmos {
+
+	public static int fibonacci(int num1) {
+
+		if (num1 <= 1) {
+
+			return num1;
+
+		} else {
+
+			return fibonacci(num1 - 1) + fibonacci(num1 - 2);
+
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param numero
+	 * @return
+	 */
+
+	public static int factorial(int numero) {
+
+		return 0;
+
+	}
+
+	public static String esPrimo(int numero) {
+
+		return "";
+
+	}
+
+}
