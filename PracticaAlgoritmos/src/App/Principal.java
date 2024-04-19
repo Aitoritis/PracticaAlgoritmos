44d008da0b773171a68a99debe587113b2f01c96
Aitoritis <alb0026@alu.medac.es>
2024-04-19 19:25:25 +0200

    Clase Main

---
+++ PracticaAlgoritmos/src/App/Principal.java
@@ -0,0 +1,42 @@
+package App;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		// Creamos los arrays.
+
+		int[] numeros = new int[5];
+		int[] resultadoFibonacci = new int[5];
+		int[] resultadoFactorial = new int[5];
+		boolean[] resultadoPrimos = new boolean[5];
+		
+		// Se realizan sus respectivos for para calcularlos.
+
+		for (int i = 0; i < numeros.length; i++) {
+
+			numeros[i] = (int) (Math.random() * 10 + 1);
+
+		}
+
+		for (int i = 0; i < resultadoFibonacci.length; i++) {
+
+			resultadoFibonacci[i] = Util.Algoritmos.fibonacci(numeros[i]);
+
+		}
+
+		for (int i = 0; i < resultadoFactorial.length; i++) {
+
+			resultadoFactorial[i] = Util.Algoritmos.factorial(numeros[i]);
+
+		}
+
+		for (int i = 0; i < resultadoPrimos.length; i++) {
+
+			resultadoPrimos[i] = Util.Algoritmos.esPrimo(numeros[i]);
+
+		}
+
+	}
+
+}
